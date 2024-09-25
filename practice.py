dee24b0a6679fb8ae9aa3bdc14e5afbbdfb28040
Brayden-Tracy <btracy4@uwyo.edu>
2024-09-25 22:27:58 +0000

    did the assignment

---
+++ practice.py
@@ -0,0 +1,11 @@
+names = ("Brayden", "Brandon", "Champ","Lily","Huston","Koen")
+print(names)
+
+del (names[0])
+print(names)
+
+for names in names:
+    print(f"{name} is on my table")
+
+names.reverse()
+print(names)
